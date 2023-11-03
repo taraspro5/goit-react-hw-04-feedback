e32e4bb64e578d71af02c9af21ee2778b9b7b1fa
taraspro5 <kovtun_taras007@ukr.net>
2023-11-03 22:00:22 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
